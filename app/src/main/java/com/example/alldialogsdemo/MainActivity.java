b17a3481b9c421ba395a84464df7ddc993a2a05d
abdullahwale <abdullahwale@gmai.com>
2020-03-05 15:59:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/alldialogsdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.alldialogsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog() {
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+}
